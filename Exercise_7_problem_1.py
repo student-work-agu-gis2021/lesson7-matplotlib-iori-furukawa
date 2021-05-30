9a1d19976548660bd7e8f6ac914b9abf37797b2a
iorifurukawa <>
2021-05-30 15:49:20 +0000

    Create a new data frame and add random numbers to x and y.

--- Exercise_7_problem_1.py
+++ Exercise_7_problem_1.py
@@ -19,6 +19,11 @@ import numpy as np
 import pandas as pd
 
 # YOUR CODE HERE 1 to set data
+data=pd.DataFrame()
+x=np.random.rand(1000)
+y=np.random.rand(1000)
+data["x"]=x
+data["y"]=y
 
 # Check your random values
 print(data.head())
