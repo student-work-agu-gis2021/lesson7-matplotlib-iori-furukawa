d350421edadcd0fcda2456bf628937a953461328
iorifurukawa <>
2021-05-30 16:04:24 +0000

    Set the colors and import matplotlib of the scatter.

--- Exercise_7_problem_1.py
+++ Exercise_7_problem_1.py
@@ -58,6 +58,8 @@ assert len(colors) == 1000, "There should be 1000 random numbers for colors"
 
 # Plot a scatter plot
 # YOUR CODE HERE 3
+import matplotlib.pyplot as plt
+plt.scatter(x,y,s=50,c=colors,cmap='rainbow',edgecolor='black')
 
 # Add labels and title
 # YOUR CODE HERE 4
